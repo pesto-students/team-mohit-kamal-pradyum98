3984ed3c44f58fb61f38d7fce9349c1c6a0d9e64
pradyum-loop <pradyum.menon@loophealth.com>
2023-06-22 19:23:20 +0530

    Requested changes

--- Week-3/Session-9/Arrays/script.js
+++ Week-3/Session-9/Arrays/script.js
@@ -1,9 +1,12 @@
 const divideArray = (numbers) => {
-    console.log('Even numbers:')
-    const even = numbers.filter((e) => e % 2 == 0).sort().forEach((e) => console.log(e));
+    console.log('Even numbers:');
+    const even = numbers
+        .filter((e) => e % 2 == 0)
+        .sort((a, b) => a - b)
+        .forEach((e) => console.log(e));
     console.log('Odd numbers:');
     const odd = numbers
         .filter((e) => e % 2 == 1)
-        .sort()
+        .sort((a, b) => a - b)
         .forEach((e) => console.log(e));
-}
+};
