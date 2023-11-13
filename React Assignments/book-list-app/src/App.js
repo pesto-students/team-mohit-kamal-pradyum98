d1bd24018d0cb0534fe0e45a5b2dd261d741eb35
pradyum-loop <pradyum.menon@loophealth.com>
2023-11-13 09:19:14 +0530

    React Week-1 Assignments

---
+++ React Assignments/book-list-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from './components/BookList/BookList';
+
+const App = () => {
+    return (
+        <div>
+            <BookList />
+        </div>
+    );
+};
+
+export default App;
